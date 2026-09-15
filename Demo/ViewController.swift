2f645886bfb73a3164353447e30d03b590491bc6
tanglei11 <521017818@qq.com>
2026-09-15 19:39:17 +0800

    移除部分文件

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -12,40 +12,13 @@ class ViewController: UIViewController {
 
     private let selectButton = UIButton(type: .system)
     
-    private lazy var studyButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("学习", for: .normal)
-        button.addTarget(self, action: #selector(handleStudy), for: .touchUpInside)
-        return button
-    }()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        view.addSubview(studyButton)
-        studyButton.snp.makeConstraints { maker in
-            maker.center.equalToSuperview()
-            maker.width.equalTo(120)
-            maker.height.equalTo(44)
-        }
-        
-//        if let path = Bundle.main.path(forResource: "career_db", ofType: "sqlite") {
-//            print("✅ 数据库文件存在，路径：\(path)")
-//        } else {
-//            print("❌ 数据库文件未找到，请检查导入和文件名大小写")
-//        }
-        
-        
-//        BluetoothManager.shared.start()
-        
-//        setupSelectButton()
+        setupSelectButton()
     }
     
-    deinit {
-        BluetoothManager.shared.stopScan()
-    }
-
     private func setupSelectButton() {
         selectButton.setTitle("选择图片", for: .normal)
         selectButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .medium)
@@ -89,21 +62,6 @@ extension ViewController: PHPickerViewControllerDelegate {
             DispatchQueue.main.async {
                 self?.openPreview(with: image)
             }
-        }
-    }
-}
-
-extension ViewController {
-    
-    @objc private func handleStudy() {
-//        let controller = StudyController()
-//        navigationController?.pushViewController(controller, animated: true)
-        
-        if #available(iOS 17.0, *) {
-            let controller = ObjectCaptureController()
-            navigationController?.pushViewController(controller, animated: true)
-        } else {
-            // Fallback on earlier versions
         }
     }
 }
